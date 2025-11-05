f0dbaeb18a63ddf36a35b1ccf87658e47626b3af
Zoltán Magyar <zoltanmagyar28@icloud.com>
2025-11-05 12:36:10 +0100

    added docc plugin but commented out for now

--- Package.swift
+++ Package.swift
@@ -10,6 +10,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0")
+        //.package(url: "https://github.com/apple/swift-docc-plugin", from: "1.4.0"),
     ],
     targets: [
         // Minimal system library wrapper for OpenConnect
