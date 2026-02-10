01690cdde12927a7de2ad5849f247626c3b8a56e
Zoltán Magyar <zoltanmagyar28@icloud.com>
2026-02-10 13:03:06 +0100

    OpenConnectKit moved outside of the repository

--- Package.swift
+++ Package.swift
@@ -5,44 +5,22 @@ let package = Package(
   name: "SwiftConnectCli",
   platforms: [.macOS(.v26)],
   products: [
-    .library(name: "OpenConnectKit", targets: ["OpenConnectKit"]),
-    .executable(name: "swiftconnect-cli", targets: ["SwiftConnectCli"]),
+    .executable(name: "swiftconnect-cli", targets: ["SwiftConnectCli"])
   ],
   dependencies: [
-    .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0")
+    .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
+    .package(path: "../OpenConnectKit"),
   ],
   targets: [
-    // Minimal system library wrapper for OpenConnect
-    .systemLibrary(
-      name: "COpenConnectLib",
-      pkgConfig: "openconnect",
-      providers: [
-        .brew(["openconnect"]),
-        .apt(["libopenconnect-dev"]),
-      ]
-    ),
-    // C wrapper target for shims that depend on OpenConnect
-    .target(
-      name: "COpenConnect",
-      dependencies: ["COpenConnectLib"]
-    ),
-    // Swift library wrapping OpenConnect with Swift-native API
-    .target(
-      name: "OpenConnectKit",
-      dependencies: ["COpenConnect"],
-      swiftSettings: [
-        .enableUpcomingFeature("NonisolatedNonsendingByDefault")
-      ]
-    ),
     .executableTarget(
       name: "SwiftConnectCli",
       dependencies: [
-        "OpenConnectKit",
+        .product(name: "OpenConnectKit", package: "OpenConnectKit"),
         .product(name: "ArgumentParser", package: "swift-argument-parser"),
       ],
       swiftSettings: [
         .enableUpcomingFeature("NonisolatedNonsendingByDefault")
       ]
-    ),
+    )
   ]
 )
